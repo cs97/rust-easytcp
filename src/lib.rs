94c9f92dd1b7c33658b69f752b06f80ccbc4fea6
cs97 <chrswr97@gmail.com>
2022-10-16 22:45:30 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -196,7 +196,7 @@ pub mod tcp_aes_cbc {
 			return Ok(());
 		}
 		pub fn recive(&self) -> std::io::Result<Vec<u8>> {
-			return Ok(dec256cbc(self.tcp_conn.recive()?, self.key)?);
+			return Ok(dec256cbc(self.tcp_conn.recive()?, self.key)[16..]?);
 		}
 	}
 
